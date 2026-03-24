997be93d91d8df754be079de054031c29cadd2fd
Thomas-dev01 <lol.fueguino@gmail.com>
2026-03-24 00:02:28 -0300

    organizacion de archivos

--- actividad4.js
+++
@@ -1,9 +0,0 @@
-let temperatura = Number(prompt("cual es la temperatura actual?"));
-if (temperatura < 10) {
-    alert("hace frio")
-}
-else if (temperatura >= 10 && temperatura <= 25) {
-    alert("esta templado")
-}
-else 
-    alert("hace calor");
